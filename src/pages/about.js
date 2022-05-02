99d462658f133823688c80fca39793f49894aab2
Monday_David_Sarka <david_md@hamari.agency>
2022-05-02 23:49:35 +0100

    Learnt how to create a page component from scratch

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+const AboutPage = () => {
+    return (
+        <main>
+            <title>About Me</title>
+            <h1>About OnlineMD</h1>
+            <p>Hi, I created this Site myself Using React and its Gatsby Library.</p>
+        </main>
+    )
+}
+
+export default AboutPage
