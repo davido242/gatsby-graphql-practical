869914027dfd1d32cc2577b1f0ec7f2474948528
Monday_David_Sarka <david_md@hamari.agency>
2022-05-09 18:20:39 +0100

    added nodeType and filed slug page for dynamic page rendering

---
+++ src/pages/blog/{mdx.slug}.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Layout from "../../components/layout"
+
+const BlogPost = () => {
+    return (
+        <Layout pageTitle="Super Cool Blog Posts with its node and slug fileds">
+            <p>My bLog Post</p>
+        </Layout>
+    )
+}
+
+export default BlogPost
