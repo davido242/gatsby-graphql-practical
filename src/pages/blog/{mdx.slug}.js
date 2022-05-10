294c8d5561c7c09da2fc4a592485ccec624152bb
Monday_David_Sarka <david_md@hamari.agency>
2022-05-10 16:32:18 +0100

    Used queried variable to render post on seperate pages

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -1,12 +1,29 @@
+import { graphql } from "gatsby";
 import * as React from "react";
 import Layout from "../../components/layout"
+import { MDXRenderer } from 'gatsby-plugin-mdx'
 
-const BlogPost = () => {
+const BlogPost = ({ data }) => {
     return (
-        <Layout pageTitle="Super Cool Blog Posts with its node and slug fileds">
-            <p>My bLog Post</p>
+        <Layout pageTitle={data.mdx.frontmatter.title}>
+            <p>{data.mdx.frontmatter.date}</p>
+            <MDXRenderer>
+                {data.mdx.body}
+            </MDXRenderer>
         </Layout>
     )
 }
+
+export const query = graphql` 
+    query ($id: String) {
+        mdx(id: {eq: $id}) {
+        frontmatter {
+            title
+            date(formatString: "MM DD YYYY")
+        }
+        body
+        }
+      }
+    `
 
 export default BlogPost
